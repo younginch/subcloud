a249ab34136eb7a30262b6bc3dc127b6fc74b326
Seungbin Oh <sboh1214@gmail.com>
2022-04-10 17:26:24 +0900

    add google login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
 const prisma = new PrismaClient();
@@ -11,6 +12,10 @@ export default NextAuth({
     signIn: "/auth/signin",
   },
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -3,12 +3,23 @@ import type { Provider } from "next-auth/providers";
 import { getProviders, signIn } from "next-auth/react";
 import type { GetServerSidePropsContext } from "next/types";
 import Layout from "../../components/layout";
-import { FaGithub } from "react-icons/fa";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 import { useRouter } from "next/router";
 
 type Props = {
   providers: Provider[];
 };
+
+function getIcon(id: string) {
+  switch (id) {
+    case "google":
+      return <FaGoogle />;
+    case "github":
+      return <FaGithub />;
+    default:
+      return;
+  }
+}
 
 export default function SignIn({ providers }: Props) {
   const router = useRouter();
@@ -22,7 +33,7 @@ export default function SignIn({ providers }: Props) {
             {Object.values(providers).map((provider) => (
               <div key={provider.name}>
                 <Button
-                  leftIcon={<FaGithub />}
+                  leftIcon={getIcon(provider.id)}
                   onClick={() =>
                     signIn(provider.id, {
                       callbackUrl: router.query["callbackUrl"] as string,
