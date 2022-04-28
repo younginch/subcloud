e35660d50591cb239e1153a2c9a1d2355ff167e4
Seungbin Oh <sboh1214@gmail.com>
2022-04-28 14:02:24 +0900

    add video detail

---
+++ src/pages/api/video/[...ids].ts
@@ -0,0 +1,28 @@
+import { PrismaClient, Video } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import ResError from "../../../utils/types";
+
+export default async function Video(
+  req: NextApiRequest,
+  res: NextApiResponse<Video | ResError>
+) {
+  const serviceId = req.query.ids[0];
+  const videoId = req.query.ids[1];
+  if (!serviceId || !videoId) {
+    return res.status(400).json({ error: "Missing id" });
+  }
+  const prisma = new PrismaClient();
+  if (req.method === "GET") {
+    try {
+      const video = await prisma.video.findUnique({
+        where: { serviceId_videoId: { serviceId, videoId } },
+      });
+      if (!video) {
+        return res.status(404).json({ error: "Video not found" });
+      }
+      return res.status(200).json(video);
+    } catch (e: any) {
+      return res.status(500).json({ error: e.message });
+    }
+  }
+}
