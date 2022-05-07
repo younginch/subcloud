e37f3b462c59ee65a348142c6d4b60055f059bf8
Seungbin Oh <sboh1214@gmail.com>
2022-05-08 02:48:13 +0900

    add list components

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,28 +1,41 @@
-import { Heading, ListItem } from "@chakra-ui/react";
+import { Heading, List, ListItem } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 type JangJoProps = {
-  children: string;
+  index: number;
+  title: string;
+  children: ReactNode;
 };
 
-export function Jang({ children }: JangJoProps) {
+export function Jang({ index, title, children }: JangJoProps) {
   return (
-    <Heading size="lg" marginTop="18px" marginBottom="12px">
-      {children}
-    </Heading>
+    <>
+      <Heading
+        size="lg"
+        marginTop="18px"
+        marginBottom="12px"
+      >{`제${index}장 ${title}`}</Heading>
+      <List>{children}</List>
+    </>
   );
 }
 
-export function Jo({ children }: JangJoProps) {
+export function Jo({ index, title, children }: JangJoProps) {
   return (
-    <Heading size="md" marginTop="12px" marginBottom="6px">
-      {children}
-    </Heading>
+    <ListItem>
+      <Heading
+        size="md"
+        marginTop="12px"
+        marginBottom="6px"
+      >{`제${index}조 [${title}]`}</Heading>
+      {Array.isArray(children) ? <List>{children}</List> : children}
+    </ListItem>
   );
 }
 
 type HangProps = {
-  children: string;
   index: number;
+  children: ReactNode;
 };
 
 const circledNumbers = ["", "\u2460", "\u2461", "\u2462", "\u2463"];
