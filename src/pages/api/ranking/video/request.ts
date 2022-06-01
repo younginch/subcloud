677cd27899f6a4f194086723ad9a8751226abe94
myeongjinshin <hanwoolmj2@gmail.com>
2022-06-02 05:48:07 +0900

    Add subtitle view ranking

---
+++ src/pages/api/ranking/video/request.ts
@@ -0,0 +1,44 @@
+import {
+  handleRoute,
+  ResRankingSub,
+  RouteParams,
+  SubErrorType,
+} from "../../../../utils/types";
+
+async function RankingVideoByRequest({
+  req,
+  res,
+  prisma,
+}: RouteParams<ResRankingSub>) {
+  const { lang, start, end } = req.query;
+  if (!start && !end) {
+    return res
+      .status(400)
+      .json({ error: SubErrorType.FormValidation, message: "FormInvalidated" });
+  }
+  let where: any = {};
+  if (lang) {
+    where.lang = lang;
+  }
+  const subs = await prisma.sub.findMany({
+    take: Number(end),
+    orderBy: [
+      {
+        views: "desc",
+      },
+    ],
+    where,
+    include: {
+      video: { include: { youtubeVideo: { include: { channel: true } } } },
+      user: true,
+    },
+  });
+  if (!subs) {
+    return res
+      .status(404)
+      .json({ error: SubErrorType.NotFound, message: "RankingSubView" });
+  }
+  return res.status(200).json(subs.slice(Number(start)));
+}
+
+export default handleRoute({ GET: RankingVideoByRequest });
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -207,6 +207,10 @@ type SubWithFileUrl = Sub & {
   url: string;
 };
 
+type SubWithVideoWithUser = SubWithVideo & {
+  user: User;
+};
+
 export type ResFileRead = FileWithUrl;
 export type ResFileDelete = File;
 export type ResFileSearch = File[];
@@ -221,3 +225,6 @@ export type ResSubView = Sub;
 export type ResUser = User;
 export type ResVideo = VideoWithInfo;
 export type ResVideoSearch = VideoWithCount[];
+export type ResRankingSub = SubWithVideoWithUser[];
+export type ResRankingVideo = Video[];
+export type ResRankingUser = User[];
