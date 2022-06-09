5809e86c6bcd6afa854390fe87111319b310373e
myeongjinshin <hanwoolmj2@gmail.com>
2022-06-09 18:22:34 +0900

    Add example video

---
+++ src/pages/api/video/example.ts
@@ -0,0 +1,12 @@
+import { handleRoute, RouteParams } from "../../../utils/types";
+import { Role, ExampleVideo } from "@prisma/client";
+
+async function getExampleVideo({ res, prisma }: RouteParams<ExampleVideo>) {
+  const videos = await prisma.exampleVideo.findMany({
+    include: { video: true },
+  });
+  const randomIndex = Math.floor(Math.random() * videos.length);
+  return res.status(200).json(videos[randomIndex]);
+}
+
+export default handleRoute({ GET: getExampleVideo }, { role: Role.User });
