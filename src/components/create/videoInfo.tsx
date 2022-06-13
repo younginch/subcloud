dfb1a9e57e11d8c44123f98e581dcfda3c98d617
Mingyu Lee <minkyu.lee65@gmail.com>
2022-06-13 21:53:50 +0900

    improve request design

--- src/components/create/videoInfo.tsx
+++ src/components/create/videoInfo.tsx
@@ -6,6 +6,8 @@ import {
   Avatar,
   HStack,
   Stack,
+  Flex,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -18,6 +20,7 @@ type Props = {
 };
 
 export default function VideoInfo({ serviceId, videoId }: Props) {
+  const [isLargerThan1280] = useMediaQuery("(min-width: 900px)");
   const [video, setVideo] = useState<ResVideo>();
   useEffect(() => {
     axios
@@ -28,38 +31,38 @@ export default function VideoInfo({ serviceId, videoId }: Props) {
   }, [serviceId, videoId]);
 
   return (
-    <Stack>
-      <VideoForm value={video?.url} />
-      <Box maxW="28em" borderWidth={1} borderRadius="6px" overflow="hidden">
-        <Image
-          maxW="28em"
-          marginEnd="24px"
-          src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
-          alt="Video thumbnail"
-        />
-        {video?.youtubeVideo ? (
-          <Box padding="12px">
-            <Heading size="md">{video?.youtubeVideo.title}</Heading>
-            <Text marginY="12px">
-              {`조회수 ${video?.youtubeVideo.viewCount}회 | ${video?.youtubeVideo.publishedAt}`}
-            </Text>
-            <HStack>
-              <Avatar
-                marginEnd="12px"
-                src={video?.youtubeVideo.channel.thumbnailUrl}
-              />
-              <Stack>
-                <Heading size="sm">{video?.youtubeVideo.channel.title}</Heading>
-                <Text>
-                  구독자 {video?.youtubeVideo.channel.subscriberCount}명
-                </Text>
-              </Stack>
-            </HStack>
-          </Box>
-        ) : (
-          <Text>정보 없음</Text>
-        )}
-      </Box>
-    </Stack>
+    <Flex
+      direction={isLargerThan1280 ? "row" : "column"}
+      overflow="hidden"
+      alignItems="center"
+    >
+      <Image
+        maxW="28em"
+        src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
+        alt="Video thumbnail"
+      />
+      {video?.youtubeVideo ? (
+        <Box padding="12px">
+          <Heading size="md">{video?.youtubeVideo.title}</Heading>
+          <Text marginY="10px">
+            {`조회수 ${video?.youtubeVideo.viewCount}회 | ${video?.youtubeVideo.publishedAt}`}
+          </Text>
+          <HStack>
+            <Avatar
+              marginEnd="12px"
+              src={video?.youtubeVideo.channel.thumbnailUrl}
+            />
+            <Stack>
+              <Heading size="sm">{video?.youtubeVideo.channel.title}</Heading>
+              <Text>
+                구독자 {video?.youtubeVideo.channel.subscriberCount}명
+              </Text>
+            </Stack>
+          </HStack>
+        </Box>
+      ) : (
+        <Text>정보 없음</Text>
+      )}
+    </Flex>
   );
 }
--- src/pages/video/[serviceId]/[videoId]/request/create.tsx
+++ src/pages/video/[serviceId]/[videoId]/request/create.tsx
@@ -5,8 +5,26 @@ import {
   FormErrorMessage,
   Button,
   useToast,
-  Wrap,
   WrapItem,
+  Stack,
+  Flex,
+  Text,
+  useColorModeValue,
+  MenuButton,
+  Menu,
+  MenuList,
+  MenuItem,
+  Wrap,
+  Center,
+  Slider,
+  SliderFilledTrack,
+  SliderTrack,
+  SliderThumb,
+  Box,
+  HStack,
+  useMediaQuery,
+  Checkbox,
+  MenuOptionGroup,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -17,8 +35,11 @@ import { RequestCreateSchema } from "../../../../../utils/schema";
 import { useSession } from "next-auth/react";
 import { Role } from "@prisma/client";
 import VideoInfo from "../../../../../components/create/videoInfo";
-import CreateHeader from "../../../../../components/create/createHeader";
 import { PageOptions } from "../../../../../utils/types";
+import Card from "../../../../../components/user/card/card";
+import CardHeader from "../../../../../components/user/card/cardHeader";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import InViewProvider from "../../../../../components/inviewProvider";
 
 type FormData = {
   serviceId: string;
@@ -28,6 +49,10 @@ type FormData = {
 };
 
 export default function RequestCreate() {
+  const textColor = useColorModeValue("gray.700", "gray.300");
+  const subTextColor = useColorModeValue("gray.600", "gray.400");
+  const [summaryToggle] = useMediaQuery("(min-width: 1450px)");
+  const [videoInfoToggle] = useMediaQuery("(min-width: 900px)");
   const router = useRouter();
   const serviceId = router.query.serviceId as string;
   const videoId = router.query.videoId as string;
@@ -38,7 +63,15 @@ export default function RequestCreate() {
     register,
     formState: { errors, isSubmitting },
   } = useForm<FormData>({ resolver: joiResolver(RequestCreateSchema) });
-
+  const pointArray = [10, 50, 100, 500, 1000, 5000];
+  const pointArrayColor = [
+    "red.200",
+    "green.200",
+    "tomato",
+    "blue.200",
+    "gray.200",
+    "yellow",
+  ];
   function onSubmit(values: FormData) {
     return new Promise<void>((resolve, reject) => {
       axios
@@ -69,68 +102,174 @@ export default function RequestCreate() {
   }
 
   return (
-    <>
-      <CreateHeader type="request" step={2} />
-      <Wrap>
-        <WrapItem>
-          <VideoInfo serviceId={serviceId} videoId={videoId} />
-        </WrapItem>
-        <WrapItem paddingX="36px">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl isInvalid={errors.serviceId !== undefined} hidden>
-              <FormLabel htmlFor="serviceId">서비스</FormLabel>
-              <Input
-                id="serviceId"
-                value={router.query.serviceId}
-                {...register("serviceId")}
-              />
-              <FormErrorMessage>
-                {errors.serviceId && errors.serviceId.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.videoId !== undefined} hidden>
-              <FormLabel htmlFor="videoId">영상 ID</FormLabel>
-              <Input
-                id="videoId"
-                value={router.query.videoId}
-                {...register("videoId")}
-              />
-              <FormErrorMessage>
-                {errors.videoId && errors.videoId.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl as="fieldset">
-              <FormLabel as="legend">요청할 자막 언어</FormLabel>
-              <SelectLanguage register={register("lang")} />
-              <FormErrorMessage>
-                {errors.lang && errors.lang.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.point !== undefined}>
-              <FormLabel htmlFor="point">포인트</FormLabel>
-              <Input
-                id="point"
-                value={router.query.point}
-                type="number"
-                {...register("point")}
-              />
-              <FormErrorMessage>
-                {errors.point && errors.point.message}
-              </FormErrorMessage>
-            </FormControl>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={isSubmitting}
-              type="submit"
-            >
-              요청
-            </Button>
-          </form>
-        </WrapItem>
-      </Wrap>
-    </>
+    <Stack ms={{ base: "20px", xl: "calc(15vw - 150px)" }} spacing={5}>
+      <Card
+        w={videoInfoToggle ? "fit-content" : "calc(100vw - 40px)"}
+        mt={5}
+        maxW="calc(100vw - 40px)"
+      >
+        <CardHeader mb="10px">
+          <Text color={textColor} fontSize="lg" fontWeight="bold" mb="4px">
+            요청 영상
+          </Text>
+        </CardHeader>
+        <VideoInfo serviceId={serviceId} videoId={videoId} />
+      </Card>
+      <Card w="850px" mt={5} zIndex={2} maxW="calc(100vw - 40px)">
+        <CardHeader mb="24px">
+          <Text color={textColor} fontSize="lg" fontWeight="bold" mb="4px">
+            언어 선택
+          </Text>
+        </CardHeader>
+        <Menu>
+          <MenuOptionGroup>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              언어 선택
+            </MenuButton>
+            <MenuList>
+              <MenuItem>한국어</MenuItem>
+              <MenuItem>구자라트어</MenuItem>
+              <MenuItem>영어</MenuItem>
+              <MenuItem>프랑스어</MenuItem>
+              <MenuItem>스페인어</MenuItem>
+            </MenuList>
+          </MenuOptionGroup>
+        </Menu>
+        <Checkbox mt={5} size="lg" defaultChecked>
+          기본 요청 언어로 저장
+        </Checkbox>
+      </Card>
+      <Card w="850px" mt={5} maxW="calc(100vw - 40px)">
+        <CardHeader mb="24px">
+          <Text color={textColor} fontSize="lg" fontWeight="bold" mb="4px">
+            포인트
+          </Text>
+        </CardHeader>
+        <Wrap>
+          {pointArray.map((value, index) => {
+            return (
+              <WrapItem key={index}>
+                <InViewProvider whileHover={1.05} initialScale={0.95}>
+                  <Center
+                    w="80px"
+                    h="80px"
+                    borderRadius="20%"
+                    bg={pointArrayColor[index]}
+                  >
+                    {value}P
+                  </Center>
+                </InViewProvider>
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+        <HStack pt={5}>
+          <Slider defaultValue={60} min={0} max={300} step={30}>
+            <SliderTrack bg="red.100">
+              <Box position="relative" right={10} />
+              <SliderFilledTrack bg="tomato" />
+            </SliderTrack>
+            <SliderThumb boxSize={6} />
+          </Slider>
+          <Input placeholder="Basic usage" />
+        </HStack>
+      </Card>
+      <Card
+        w="500px"
+        className={summaryToggle ? "requestFixed" : "requestBottom"}
+        zIndex={3}
+        maxW="calc(100vw - 40px)"
+      >
+        <CardHeader mb="24px">
+          <Text color={textColor} fontSize="lg" fontWeight="bold" mb="4px">
+            요청 요약
+          </Text>
+        </CardHeader>
+        <Text fontSize="15px">영상 제목</Text>
+        <Text fontWeight="bold" fontSize="20px">
+          펜 윅과 명 윅
+        </Text>
+        <Text fontSize="15px" mt="20px">
+          영상 길이
+        </Text>
+        <Text fontWeight="bold" fontSize="20px">
+          22분 20초
+        </Text>
+        <Text fontSize="15px" mt="20px">
+          요청 언어
+        </Text>
+        <Text fontWeight="bold" fontSize="20px">
+          한국어
+        </Text>
+        <Text fontSize="15px" mt="20px">
+          제공 포인트
+        </Text>
+        <Text fontWeight="bold" fontSize="20px">
+          123
+        </Text>
+        <Button colorScheme="blue" mt="20px">
+          요청 올리기
+        </Button>
+      </Card>
+      <WrapItem paddingX="36px">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={errors.serviceId !== undefined} hidden>
+            <FormLabel htmlFor="serviceId">서비스</FormLabel>
+            <Input
+              id="serviceId"
+              value={router.query.serviceId}
+              {...register("serviceId")}
+            />
+            <FormErrorMessage>
+              {errors.serviceId && errors.serviceId.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.videoId !== undefined} hidden>
+            <FormLabel htmlFor="videoId">영상 ID</FormLabel>
+            <Input
+              id="videoId"
+              value={router.query.videoId}
+              {...register("videoId")}
+            />
+            <FormErrorMessage>
+              {errors.videoId && errors.videoId.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl as="fieldset">
+            <FormLabel as="legend">요청할 자막 언어</FormLabel>
+            <SelectLanguage register={register("lang")} />
+            <FormErrorMessage>
+              {errors.lang && errors.lang.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.point !== undefined}>
+            <FormLabel htmlFor="point">포인트</FormLabel>
+            <Input
+              id="point"
+              value={router.query.point}
+              type="number"
+              {...register("point")}
+            />
+            <FormErrorMessage>
+              {errors.point && errors.point.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            mt={4}
+            colorScheme="teal"
+            isLoading={isSubmitting}
+            type="submit"
+          >
+            요청
+          </Button>
+        </form>
+      </WrapItem>
+    </Stack>
   );
 }
 
-RequestCreate.options = { role: Role.User, hideTitle: true } as PageOptions;
+RequestCreate.options = {
+  role: Role.User,
+  hideTitle: true,
+  bgColorLight: "#F7FAFC",
+} as PageOptions;
