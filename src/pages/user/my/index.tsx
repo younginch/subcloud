de7984fbb917afd5ba3ee04b936117cd87d2efb1
Mingyu Lee <minkyu.lee65@gmail.com>
2022-06-17 17:42:47 +0900

    reformat profile modal

--- src/pages/user/my/index.tsx
+++ src/pages/user/my/index.tsx
@@ -1,8 +1,17 @@
-import { Avatar, Heading, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { Role, User } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import useSWR from "swr";
 import { PageOptions } from "../../../utils/types";
+import Link from "next/link";
 
 export default function UserMyIndex() {
   const session = useSession();
@@ -24,6 +33,21 @@ export default function UserMyIndex() {
           <Text>{data?.point} 포인트</Text>
         </Stack>
       </HStack>
+      <Link href={`/user/${session.data?.user.id}`} passHref>
+        <Button
+          w={"full"}
+          mt={4}
+          bg={useColorModeValue("#151f21", "gray.900")}
+          color={"white"}
+          rounded={"md"}
+          _hover={{
+            transform: "translateY(-2px)",
+            boxShadow: "lg",
+          }}
+        >
+          내 퍼블릭 프로필
+        </Button>
+      </Link>
     </Stack>
   );
 }
