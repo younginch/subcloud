5857293924002231ec5bd38ed86d6d544f4fd600
Seungbin Oh <sboh1214@gmail.com>
2022-06-20 12:55:12 +0900

    Add component test

--- src/__tests__/component.test.tsx
+++ src/__tests__/component.test.tsx
@@ -1,7 +1,13 @@
+import { SubStatus } from "@prisma/client";
 import { render } from "@testing-library/react";
+import AdminLayout from "../components/adminLayout";
+import ReviewStatusBadge from "../components/badges/reviewStatusBadge";
 import Links from "../components/header/links";
 import Search from "../components/header/search";
+import Layout from "../components/layout";
+import SelectLanguage from "../components/selectLanguage";
 import RequestPanel from "../components/user/requestPanel";
+import { renderWithThemeAndSession } from "../utils/jest";
 
 describe("Components", () => {
   it("renders a Footer", () => {
@@ -14,5 +20,44 @@ describe("Components", () => {
 
   it("renders a RequestPanel", () => {
     render(<RequestPanel requests={[]} />);
+  });
+
+  it("renders a adminLayout", () => {
+    render(<AdminLayout>a</AdminLayout>);
+  });
+
+  it("renders a Layout", () => {
+    renderWithThemeAndSession(
+      <Layout
+        options={{
+          auth: false,
+        }}
+      >
+        a
+      </Layout>
+    );
+  });
+
+  it("renders a SelectLanguage", () => {
+    render(
+      <SelectLanguage
+        register={{
+          onChange: jest.fn(),
+          onBlur: jest.fn(),
+          ref: jest.fn(),
+          name: "",
+        }}
+      />
+    );
+  });
+
+  it("renders ReviewStatusBadge", () => {
+    render(<ReviewStatusBadge />);
+    render(<ReviewStatusBadge status={SubStatus.Approved} />);
+    render(<ReviewStatusBadge status={SubStatus.InReview} />);
+    render(<ReviewStatusBadge status={SubStatus.Pending} />);
+    render(<ReviewStatusBadge status={SubStatus.Private} />);
+    render(<ReviewStatusBadge status={SubStatus.Rejected} />);
+    render(<ReviewStatusBadge status={SubStatus.Reported} />);
   });
 });
