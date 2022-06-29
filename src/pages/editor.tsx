f7a6311c9c53d9b057342838317bf52b4736905a
Seungbin Oh <sboh1214@gmail.com>
2022-06-29 10:23:40 +0900

    add video url dialog

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -3,31 +3,84 @@ import { PageOptions } from "../utils/types";
 import "react-reflex/styles.css";
 import YouTube from "react-youtube";
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
   Box,
   Button,
   Editable,
   EditableInput,
   EditablePreview,
+  FormControl,
   HStack,
   IconButton,
+  Input,
   Stack,
   Text,
   Textarea,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import { SRTContent, SRTFile } from "@younginch/subtitle";
 import { useDropzone } from "react-dropzone";
 import { DeleteIcon } from "@chakra-ui/icons";
+import React from "react";
 
 dayjs.extend(duration);
 
 function miliToString(mili: number): string {
-  return `${dayjs.duration(mili * 1000).format("HH:mm:ss,SSS")}`;
+  return dayjs.duration(mili * 1000).format("HH:mm:ss,SSS");
+}
+
+function AlertDialogExample() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <>
+      <Button colorScheme="red" onClick={onOpen}>
+        Delete Customer
+      </Button>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Customer
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can&apos;t undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
 }
 
 export default function Editor() {
+  const [youtubeUrl, setYoutubeUrl] = useState(
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  );
   const [contents, setContents] = useState<SRTContent[]>([]);
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles[0].text().then((text) => {
@@ -58,32 +111,37 @@ export default function Editor() {
       <ReflexElement minSize={100}>
         <HStack>
           <YouTube />
-          <Box
-            maxW="200px"
-            maxH="100px"
-            borderWidth="1px"
-            borderRadius="6px"
-            m={6}
-            {...getRootProps()}
-          >
-            <input {...getInputProps()} />
-            {isDragActive ? (
-              <p>Drop the files here ...</p>
-            ) : (
-              <p>
-                Drag &apos;n&apos; drop some files here, or click to select
-                files
-              </p>
-            )}
-          </Box>
-          <Button onClick={downloadSRT}>Save to SRT</Button>
-          <Button
-            onClick={() => {
-              setContents([]);
-            }}
-          >
-            Delete all
-          </Button>
+          <Stack>
+            <Box
+              maxW="200px"
+              maxH="100px"
+              borderWidth="1px"
+              borderRadius="6px"
+              m={6}
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p>Drop the files here ...</p>
+              ) : (
+                <p>
+                  Drag &apos;n&apos; drop some files here, or click to select
+                  files
+                </p>
+              )}
+            </Box>
+            <Button onClick={downloadSRT}>Save to SRT</Button>
+            <Button
+              onClick={() => {
+                setContents([]);
+              }}
+            >
+              Delete all
+            </Button>
+            <FormControl>
+              <Input type="url" />
+            </FormControl>
+          </Stack>
         </HStack>
       </ReflexElement>
       <ReflexSplitter />
