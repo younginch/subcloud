037c54175d0626d2d54f2fed1eab651ff372b86a
myeongjinshin <hanwoolmj2@gmail.com>
2022-07-10 23:17:35 +0900

    add sub notification

--- src/pages/api/review/sub.ts
+++ src/pages/api/review/sub.ts
@@ -1,4 +1,4 @@
-import { Role, Sub } from "@prisma/client";
+import { Role, Sub, SubStatus } from "@prisma/client";
 import { handleRoute, RouteParams } from "../../../utils/types";
 
 async function changeSub({ req, res, prisma }: RouteParams<Sub>) {
@@ -9,7 +9,37 @@ async function changeSub({ req, res, prisma }: RouteParams<Sub>) {
     where: { id: subId },
     data: { status: subStatus },
   });
-
+  if (subStatus === SubStatus.Approved) {
+    const request = await prisma.request.findUnique({
+      where: {
+        serviceId_videoId_lang: {
+          serviceId: updatedSub.serviceId,
+          videoId: updatedSub.videoId,
+          lang: updatedSub.lang,
+        },
+      },
+      include: { users: true, video: true },
+    });
+    if (request) {
+      const notice = await prisma.notice.create({
+        data: {
+          type: "Upload",
+          message: "Subtitle uploaded",
+          url: request.video.url,
+          sub: { connect: { id: updatedSub.id } },
+        },
+      });
+      request.users.map(async (user) => {
+        await prisma.notification.create({
+          data: {
+            userId: user.id,
+            noticeId: notice.id,
+            checked: false,
+          },
+        });
+      });
+    }
+  }
   return res.status(200).json(updatedSub);
 }
 
