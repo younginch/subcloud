6ac7d35f83dafdb6b63fc1aa7494f2b9b4810119
Seungbin Oh <sboh1214@gmail.com>
2022-07-21 11:19:52 +0900

    Fix lint problem on utils/types.ts

--- src/components/editor/menus.tsx
+++ src/components/editor/menus.tsx
@@ -47,33 +47,68 @@ function keysToString(sequence: KeySequence) {
     .join("");
 }
 
+type EditorMenuProps = {
+  title: string;
+  items: {
+    title: string;
+    icon?: React.ReactElement;
+    command: string;
+  }[];
+};
+
+function EditorMenu({ title, items }: EditorMenuProps) {
+  const { commandKeys, commandHandlers } = useContext(EditorContext);
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+        {title}
+      </MenuButton>
+      <MenuList zIndex={200}>
+        {items.map((item) => (
+          <MenuItem
+            key={item.title}
+            icon={item.icon}
+            command={keysToString(commandKeys[item.command])}
+            onClick={commandHandlers[item.command]}
+          >
+            {item.title}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+}
+
 export default function Menus() {
-  const { commandKeys } = useContext(EditorContext);
+  const { commandKeys, commandHandlers } = useContext(EditorContext);
 
   return (
     <HStack>
       <Link href="/" passHref>
         <Button variant="ghost">SubCloud</Button>
       </Link>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-          파일(F)
-        </MenuButton>
-        <MenuList zIndex={200}>
-          <MenuItem icon={<AddIcon />} command="">
-            새 창
-          </MenuItem>
-          <MenuItem icon={<ExternalLinkIcon />} command="N">
-            유튜브 동영상 열기...
-          </MenuItem>
-          <MenuItem icon={<EditIcon />} command="⌘O">
-            자막 파일 열기...
-          </MenuItem>
-          <MenuItem icon={<EditIcon />} command="⌘S">
-            SRT 파일로 저장...
-          </MenuItem>
-        </MenuList>
-      </Menu>
+      <EditorMenu
+        title="파일(F)"
+        items={[
+          { title: "새 창", icon: <AddIcon />, command: "" },
+          {
+            title: "유튜브 동영상 열기...",
+            icon: <ExternalLinkIcon />,
+            command: "",
+          },
+          {
+            title: "자막 파일 열기...",
+            icon: <EditIcon />,
+            command: "",
+          },
+          {
+            title: "SRT 파일로 저장...",
+            icon: <EditIcon />,
+            command: "",
+          },
+        ]}
+      />
       <Menu>
         <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
           편집(E)
@@ -82,13 +117,22 @@ export default function Menus() {
           <MenuItem
             icon={<AddIcon />}
             command={keysToString(commandKeys.NEW_SUBTITLE)}
+            onClick={commandHandlers.NEW_SUBTITLE}
           >
             현재 시간에서 새 자막
           </MenuItem>
-          <MenuItem icon={<EditIcon />} command="]">
+          <MenuItem
+            icon={<EditIcon />}
+            command={keysToString(commandKeys.CUT_SUBTITLE)}
+            onClick={commandHandlers.CUT_SUBTITLE}
+          >
             현재 시간에서 자막 끝내기
           </MenuItem>
-          <MenuItem icon={<ExternalLinkIcon />} command="⌘N">
+          <MenuItem
+            icon={<ExternalLinkIcon />}
+            command={keysToString(commandKeys.DELETE_ALL)}
+            onClick={commandHandlers.DELETE_ALL}
+          >
             모두 지우기
           </MenuItem>
           <MenuItem icon={<RepeatIcon />} command="\">
