43991e450d7c08edcc26fd2e257b89aded62253e
Seungbin Oh <sboh1214@gmail.com>
2022-07-26 13:30:25 +0900

    Add goto focused content on editor array

--- src/components/editor/menus.tsx
+++ src/components/editor/menus.tsx
@@ -227,6 +227,11 @@ export default function Menus() {
             icon: <EditIcon />,
             command: "TOGGLE_SUBTITLE_PROPERTIES",
           },
+          {
+            title: "현재 포커스된 자막으로 이동",
+            icon: <EditIcon />,
+            command: "GOTO_FOCUSED_CONTENT",
+          },
         ]}
       />
       <EditorMenu
