ffcee37b29cd0735a548e5247b093857ccb14cc1
joohyuncarter <carterkim614@younginch.com>
2022-08-03 16:10:58 +0900

    Add 'dmca' in i18n & translate whole dmca page

--- src/pages/info/dmca.tsx
+++ src/pages/info/dmca.tsx
@@ -1,99 +1,36 @@
 import { Heading, Text } from "@chakra-ui/react";
+import useTranslation from "next-translate/useTranslation";
 import { PageOptions } from "../../utils/types";
 
 export default function Dmca() {
+  const { t } = useTranslation("dmca");
   return (
     <>
-      <Heading size="lg">Content Removal Regulations</Heading>
-      <Text>
-        We want you to know that we do not have any copyrighted or illegal
-        content on SubCloud. We are in correspondence with the Digital
-        Millennium Copyright Act (“DMCA”) and general international copyright
-        laws.
-      </Text>
-      <Text>
-        However, we still can remove subtitle files from our site if the
-        copyright owner would like us to do so. The following procedure will be
-        performed if:
-      </Text>
-      <Text>Send your Claim to copyright@younginch.com</Text>
-      <Text>
-        The Claim must be written in English language, it must be understandable
-        and in polite form;
-      </Text>
-      <Text>
-        The Claim must be sent from a company mail, Claims sent from free
-        service emails like AOL, Yahoo Gmail, etc. will be rejected;
-      </Text>
-      <Text>
-        You must present an evidence that shows that you are the copyright
-        holder or that you are acting on behalf of the copyright holder;
-      </Text>
-      <Text>
-        You must present an evidence that shows that the content is legally
-        copyrighted and that you are the copyright holder;
-      </Text>
-      <Text>
-        The materials that need to be removed must be provided in form of
-        material name and links to direct subtitle pages. We do not accept links
-        to search queries, categories or subcategories or media pages.
-      </Text>
-      <Text>
-        Provide sufficient contact information with a valid email address.
-      </Text>
-      <Text>
-        And the last thing you need to know is that after your request we will
-        remove the subtitle file only from our website. Also we cannot remove
-        the torrent from other websites hosting it, even if the file has our
-        name on it or in its description.
-      </Text>
-      <Heading size="lg">Good Copyright Claim example:</Heading>
-      <Text>
-        This is a notice in accordance with the Online Copyright Infringement
-        Liability Limitation Act (OCILLA) a part of the Digital Millennium
-        Copyright Act of 1998 requesting the service providers to be held
-        strictly liable for the acts of their users and immediately cease the
-        access to copyrighted material.
-      </Text>
-      <Text>
-        We have found an infringing material in your website which indeed is our
-        youtube video “The Video” released worldwide on December 12th, 2011 and
-        we own copyright to its subtitles (transcript).
-      </Text>
-      The material that is claimed to be infringing is to be removed or access
-      to which is to be disabled immediately and avoid the growth of piracy.
-      <Text>Below are the URL for your reference:</Text>
-      <Text>Subtitle link 1</Text>
-      <Text>Subtitle link 2</Text>
-      <Text>
-        We have good faith and belief that the use of the described material in
-        the manner complained of is not authorized by the copyright owner, its
-        agent, or the law.
-      </Text>
-      <Text>
-        The information in the notification is accurate, and under penalty of
-        perjury, that the complaining party is authorized to act on behalf of
-        the owner of an exclusive right that is allegedly infringed.
-      </Text>
-      <Text>
-        We hereby declare that the information in the notification is accurate
-        to the best of our knowledge and belief.
-      </Text>
-      <Text>
-        Authorization letter of the copyright owner (or person/company
-        representing): Link to the letter
-      </Text>
-      <Text>
-        A document proving that the materials are copyrighted and belong to
-        owner (or person/company representing): Link to the document
-      </Text>
-      <Text>
-        Digital signature for copyright claim: Full info including address,
-        phone, email and website.
-      </Text>
-      <Text>
-        Copyright owners: Full info including address, phone, email and website.
-      </Text>
+      <Heading size="lg">{t("content")}</Heading>
+      <Text>{t("content_p1")}</Text>
+      <Text>{t("content_p2")}</Text>
+      <Text>{t("content_p3")}</Text>
+      <Text>{t("content_p4")}</Text>
+      <Text>{t("content_p5")}</Text>
+      <Text>{t("content_p6")}</Text>
+      <Text>{t("content_p7")}</Text>
+      <Text>{t("content_p8")}</Text>
+      <Text>{t("content_p9")}</Text>
+      <Text>{t("content_p10")}</Text>
+      <Heading size="lg">{t("copyright")}</Heading>
+      <Text>{t("copyright_p1")}</Text>
+      <Text>{t("copyright_p2")}</Text>
+      {t("copyright_p3")}
+      <Text>{t("copyright_p4")}</Text>
+      <Text>{t("copyright_p5")}</Text>
+      <Text>{t("copyright_p6")}</Text>
+      <Text>{t("copyright_p7")}</Text>
+      <Text>{t("copyright_p8")}</Text>
+      <Text>{t("copyright_p9")}</Text>
+      <Text>{t("copyright_p10")}</Text>
+      <Text>{t("copyright_p11")}</Text>
+      <Text>{t("copyright_p12")}</Text>
+      <Text>{t("copyright_p13")}</Text>
     </>
   );
 }
