3c960942eb7438ce218c675eff0247b1f9da8b1e
Mingyu Lee <minkyu.lee65@gmail.com>
2022-08-11 14:21:04 +0900

    Improve request page point

--- src/components/pointGauge.tsx
+++ src/components/pointGauge.tsx
@@ -1,17 +1,56 @@
 import { HStack, Tooltip, Box, Progress, Spacer, Text } from "@chakra-ui/react";
+import useTranslation from "next-translate/useTranslation";
 
 type Props = {
   point: number;
+  delta?: number;
   goal: number;
 };
 
-export default function PointGauge({ point, goal }: Props) {
+export default function PointGauge({ point, delta, goal }: Props) {
+  const { t } = useTranslation("badges");
+  let color = "blue";
+  if (point >= goal / 2) color = "purple";
+  if (point >= goal) color = "red";
+
+  if (!delta || point >= goal) {
+    return (
+      <HStack>
+        <Tooltip label={`${t("gauge")}: ${point}/${goal}`}>
+          <Box w="80%">
+            <Progress
+              hasStripe
+              value={(point / goal) * 100}
+              colorScheme={color}
+            />
+          </Box>
+        </Tooltip>
+        <Spacer />
+        <Text fontWeight="bold">{Math.round((point / goal) * 100)}%</Text>
+      </HStack>
+    );
+  }
   return (
     <HStack>
-      <Tooltip label={`자막 게이지: ${point}/${goal}`}>
-        <Box w="80%">
-          <Progress hasStripe value={(point / goal) * 100} />
-        </Box>
+      <Tooltip
+        label={`${t("gauge")}: ${point + delta}/${goal} (+${Math.round(
+          (delta / goal) * 100
+        )}%)`}
+      >
+        <HStack w="80%" spacing={0}>
+          <Progress
+            hasStripe
+            value={100}
+            colorScheme={color}
+            w={`${(point / goal) * 100}%`}
+          />
+          <Progress
+            hasStripe
+            value={(delta / (goal - point)) * 100}
+            colorScheme="pink"
+            w={`${100 - (point / goal) * 100}%`}
+          />
+        </HStack>
       </Tooltip>
       <Spacer />
       <Text fontWeight="bold">{Math.round((point / goal) * 100)}%</Text>
