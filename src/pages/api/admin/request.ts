9e4cba8590372ff0111f2970a8c2f9808c082ea1
myeongjinshin <hanwoolmj2@gmail.com>
2022-08-18 00:44:16 +0900

    change api admin request

--- src/pages/api/admin/request.ts
+++ src/pages/api/admin/request.ts
@@ -1,40 +1,36 @@
+/* eslint-disable no-await-in-loop */
 import { Role, Video } from "@prisma/client";
 import { handleRoute, RouteParams, SubErrorType } from "../../../utils/types";
 
-async function RequestCreate({ req, res, prisma }: RouteParams<Video[]>) {
-  const { userList, videoList, countList, langList } = req.body;
-  if (
-    videoList.length !== countList.length ||
-    videoList.length !== langList.length
-  ) {
+async function RequestCreate({ req, res, prisma }: RouteParams<Video>) {
+  const { userList, videoId, count, language } = req.body;
+  if (userList.length < count) {
     return res
       .status(404)
       .json({ error: SubErrorType.NotFound, message: "Wrong Input" });
   }
-  const videos = await videoList.map(async (videoUrl: string, i: number) => {
-    const url = new URL(videoUrl);
-    let id;
-    if (url.pathname !== "/watch") {
-      if (url.hostname === "youtu.be") {
-        [, id] = url.pathname.split("/");
-      }
-      throw new Error("Not a video url");
-    }
-    id = url.searchParams.get("v") ?? undefined;
-    const video = await prisma.video.findUnique({
-      where: { videoId: id },
+  const video = await prisma.video.findUnique({
+    where: { videoId },
+  });
+  if (!video) {
+    return res
+      .status(404)
+      .json({ error: SubErrorType.NotFound, message: "Wrong Input" });
+  }
+  const { serviceId } = video;
+  const shuffled = [...userList].sort(() => 0.5 - Math.random());
+  // eslint-disable-next-line no-restricted-syntax
+  for (const userId of shuffled.slice(0, Number(count))) {
+    const lang = language as string;
+    const request = await prisma.request.findUnique({
+      where: {
+        serviceId_videoId_lang: { serviceId, videoId, lang },
+      },
     });
-    if (!video) {
-      return res
-        .status(404)
-        .json({ error: SubErrorType.NotFound, message: "Wrong Input" });
-    }
-    const { serviceId, videoId } = video;
-    const shuffled = [...userList].sort(() => 0.5 - Math.random());
-    shuffled.slice(0, Number(countList[i])).forEach(async (userId: string) => {
+    if (request) {
       await prisma.request.update({
         where: {
-          serviceId_videoId_lang: { serviceId, videoId, lang: langList[i] },
+          serviceId_videoId_lang: { serviceId, videoId, lang },
         },
         data: {
           users: {
@@ -44,10 +40,17 @@ async function RequestCreate({ req, res, prisma }: RouteParams<Video[]>) {
           },
         },
       });
-    });
-    return video;
-  });
-  return res.status(200).json(videos);
+    } else {
+      await prisma.request.create({
+        data: {
+          video: { connect: { serviceId_videoId: { serviceId, videoId } } },
+          lang,
+          users: { connect: { id: userId } },
+        },
+      });
+    }
+  }
+  return res.status(200).json(video);
 }
 
 export default handleRoute(
